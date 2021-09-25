16b00db2929d2ccd5a374b5ff5ceefb83045aae5
daniel <menozajuan3@gmail.com>
2021-09-24 23:47:28 -0400

    mi primer repsitorio

---
+++ titulo.js
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+
+    <title>mi primera pagina web</title>
+
+
+</head>
+
+<script type= "text/javascript">
+
+      document.write('hola  mundo');
+
+</script>
+
+<body>
+
+
+
+    <h1>hola mundo</h1>
+	esta es mi primera pagina web.
+
+</body>
+
+</html>
